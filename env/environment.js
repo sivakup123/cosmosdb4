6da77b9137309a77dc02de807eea78c9258d08f3
sivakup123 <sivakup@airproducts.com>
2018-01-17 14:35:23 -0500

    Add files via upload

---
+++ env/environment.js
@@ -0,0 +1,9 @@
+const cosmosPort = 10255; // replace with your port
+const dbName = 'apcosmosdb';
+const key = 'dtFgr0AIpjjimL4MJl4SSPh2pdGSHRIR50vuN2v3ofo20W9qkJMQLjjiZygZaMpmiZKQaexRGyNTkWK8gHb7EQ==';
+
+module.exports = {
+  cosmosPort,
+  dbName,
+  key
+};
